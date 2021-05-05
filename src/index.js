4f48758613aaf571b5f1ff2daf62dadffb20b6b6
Arun Syriac <arun.syriac@outlook.com>
2021-05-05 13:56:37 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ProductContextProvider } from './contexts/product-context';
-import setupMockServer from './api/mock-server';
 import { CartContextProdiver } from './contexts/cart-context';
 import { BrowserRouter } from 'react-router-dom';
 import { WishlistProvider } from './contexts/wishlist-context';
 
-// setupMockServer();
+
 
 ReactDOM.render(
   <React.StrictMode>
