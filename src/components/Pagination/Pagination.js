8c6974e582e6ec6d16fd3ce7e43af27dfa34b86a
Arun Syriac <arun.syriac@outlook.com>
2021-07-10 21:06:30 +0530

    add pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,3 +1,5 @@
+import "./Pagination.css";
+
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -8,9 +10,9 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
     <ul className="pagination">
       {pageNumbers.map((number) => {
         return (
-          <li key={number} className="page-item">
+          <li key={number}>
             <button
-              className="page-link"
+              className="btn btn-secondary btn-sm"
               onClick={(e) => {
                 e.stopPropagation();
                 paginate(number);
