0ec3d8b07e1f71bc3801a7858f7b7529823f07bf
Arun Syriac <arun.syriac@outlook.com>
2021-07-13 02:51:16 +0530

    add form validation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { Route, Switch } from "react-router";
 import Login from "./components/Auth/Login/Login";
-import RegisterPage from "./pages/RegisterPage";
 import { PrivateRoute } from "./components/PrivateRoute";
 import CartPage from "./pages/CartPage";
 import ProductListPage from "./pages/ProductListPage";
 import WishlistPage from "./pages/WishlistPage";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import Register from "./components/Auth/Register/Register";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/" exact component={ProductListPage} />
+        <Route path="/register" exact component={Register} />
         <Route path="/login" exact component={Login} />
-        <Route path="/register" exact component={RegisterPage} />
         <PrivateRoute path="/wishlist" exact component={WishlistPage} />
         <PrivateRoute path="/cart" exact={true} component={CartPage} />
       </Switch>
